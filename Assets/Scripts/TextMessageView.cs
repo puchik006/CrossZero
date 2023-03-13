6ba870784a10e5f9c89d110b4ac517a70fc6ec58
Pavel Bu <73419843+puchik006@users.noreply.github.com>
2023-03-13 14:54:31 +0200

    Update TextMessageView.cs

--- Assets/Scripts/TextMessageView.cs
+++ Assets/Scripts/TextMessageView.cs
@@ -7,12 +7,12 @@ public class TextMessageView : MonoBehaviour
 {
     [SerializeField] private TMP_Text _gameMessage;
     [SerializeField] private GameObject _messagePanel;
-    [SerializeField] private int _showingMessageTime = 5;
+    [SerializeField] private int _messageTime = 5;
 
     public void ShowGameMessage(string message)
     {
         _gameMessage.text = message;
-        StartCoroutine(ShowMessageForSec(_showingMessageTime));
+        StartCoroutine(ShowMessageForSec(_messageTime));
     }
 
     private IEnumerator ShowMessageForSec(int sec)
