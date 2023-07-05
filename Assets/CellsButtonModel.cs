e6621237fd4d74f80bffe782cfc1d8aee5cfae1f
Pavel Bu <73419843+puchik006@users.noreply.github.com>
2023-07-05 18:38:37 +0300

    Update CellsButtonModel.cs

--- Assets/CellsButtonModel.cs
+++ Assets/CellsButtonModel.cs
@@ -5,21 +5,23 @@ public class CellsButtonModel
     private FieldValue[] _data = new FieldValue[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     public static Action<int, FieldValue> OnButtonsModelChanged;
 
+    public FieldValue[] Data { get => _data;}
+
+    private FieldValue _lastValue = FieldValue.Zero;
+
     public void ChangeModel(int number)
     {
         if (_data[number] != FieldValue.Empty) return;
 
-        _data[number] = FieldValue.Cross;
+        _data[number] = _lastValue == FieldValue.Cross ? FieldValue.Zero : FieldValue.Cross;
+        _lastValue = _data[number];
+
         OnButtonsModelChanged?.Invoke(number, _data[number]);
     }
 
     public void ClearMatrix()
     {
         Array.Clear(_data, 0, _data.Length);
-    }
-
-    private void CheckGameState()
-    {
-
+        _lastValue = FieldValue.Zero;
     }
 }
